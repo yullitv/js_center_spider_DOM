5cfb3d3200fb2a75e4b17aa1f596ce2639ddfb2f
Yullit <yuliok03@gmail.com>
2024-11-20 15:24:03 +0200

    make build

---
+++ index.109133d9.js
@@ -0,0 +1,2 @@
+var e=document.getElementsByClassName("wall")[0],t=document.getElementsByClassName("spider")[0],s=e.offsetWidth,f=e.offsetHeight,l=t.offsetWidth,o=t.offsetHeight;t.style.top="".concat((f-o)/2,"px"),t.style.left="".concat((s-l)/2,"px");
+//# sourceMappingURL=index.109133d9.js.map
